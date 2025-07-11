8d428002294d89b481aade85098337d8383d1819
federicotamburelli <federico12.tambu@gmail.com>
2025-07-11 01:46:17 -0300

    Add files via upload

---
+++ Guia Matrices/Ejer 10/ejer10.py
@@ -0,0 +1,24 @@
+Matriz = [
+[6, 4, 2], 
+[5, 10, 3],
+[9, 2, 1],
+]
+
+
+po = 0
+columna = 3
+columnasElegida = int(input("escriba el numera de la columnas q quiere sumas: "))
+
+for i in range(columna):
+    for j in range(columna): 
+        if j == columnasElegida:
+            po = po + 1
+            if po == 1:
+                num1 = Matriz[i][j]
+            if po == 2:
+                num2 = Matriz[i][j]
+            if po == 3:
+                num3 = Matriz[i][j]
+suma = num1 + num2 + num3       
+
+print("la suma de los numeros de su columna es:", suma)
